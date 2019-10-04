5b10667ced860985288f41d78bd9f7eeceedf080
OHEONG <enffl9568@naver.com>
2019-10-04 15:26:08 +0900

    초기화 안하면 인생초기화된다 잘해라

---
+++ SWEA 1865 동철이의 일 분배.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define SIZE 16
+int T, N;
+double max;
+int map[SIZE][SIZE], visited[SIZE];
+void dfs(int d, double sum) {
+	if (sum <= max) return;//와 이런생각 어떻게하지 시발 진심?
+	if (d == N) {
+		if (max < sum) max = sum;
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		if (visited[i] == 0) {
+			visited[i] = d + 1;
+			dfs(d + 1, sum*map[i][d] * 0.01);
+			visited[i] = 0;
+		}
+	}
+}
+int main() {
+	freopen("Text.txt", "r", stdin);
+	scanf("%d", &T);
+	for (int test = 0; test < T; test++) {
+		scanf("%d", &N);
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				scanf("%d", &map[i][j]);
+			}
+		}
+		dfs(0, 1.0);
+		printf("#%d %6f\n", test + 1, max * 100);
+		max = 0.0;//초기화 씨발 진짜
+	}
+	return 0;
+}
