60bafd9c7aea2b9a29c4149bd1f97c5ee897f651
OHEONG <enffl9568@naver.com>
2020-02-26 14:41:02 +0900

    #include <stdlib.h> exit(0); 활용 잘 하기

---
+++ 2239 스도쿠.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 9
+int map[SIZE][SIZE], ans[SIZE][SIZE];
+int count = 0;
+int is_ok_line(int y, int x, int num) {
+	for (int i = y - 1; 0 <= i; i--) {
+		if (map[i][x] == num) return 0;
+	}
+	for (int i = y + 1; i < SIZE; i++) {
+		if (map[i][x] == num)return 0;
+	}
+	for (int j = x - 1; 0 <= j; j--) {
+		if (map[y][j] == num)return 0;
+	}
+	for (int j = x + 1; j < SIZE; j++) {
+		if (map[y][j] == num)return 0;
+	}
+	return 1;
+}
+int is_ok_qube(int y, int x, int num) {
+	y /= 3; y *= 3;
+	x /= 3; x *= 3;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (map[y + i][x + j] == num) return 0;
+		}
+	}
+	return 1;
+}
+void dfs(int d, int y) {
+	if (d == count) {
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				printf("%d", map[i][j]);
+			}
+			printf("\n");
+		}
+		exit(0);
+		return;
+	}
+	for (int i = y; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			if (map[i][j] == 0) {
+				for (int num = 1; num <= 9; num++) {
+					if (is_ok_line(i, j, num) == 1 && is_ok_qube(i, j, num) == 1) {
+						map[i][j] = num;
+						dfs(d + 1, i);
+						map[i][j] = 0;
+					}
+				}
+			}
+			if (map[i][j] == 0) return;
+		}
+	}
+}
+int main() {
+	freopen("Text.txt", "r", stdin);
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			scanf("%1d", &map[i][j]);
+			if (map[i][j] == 0)count++;
+		}
+	}
+	dfs(0, 0);
+	return 0;
+}
+
+// 90 시간초과
