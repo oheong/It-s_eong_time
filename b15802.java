6a946d229b0476cd99a2d6ad18cc06d6e2147f42
OHEONG <enffl9568@naver.com>
2020-05-03 16:36:53 +0900

    살아남지못했다

---
+++ b15802.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class b15802 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		if("enffl9568".equals(name))System.out.println(1);
+		else System.out.println(0);
+	}
+}
