329775dbdece4e5cafbf0e2dd13fddaf6d3b1ff2
OHEONG <enffl9568@naver.com>
2020-05-16 19:03:44 +0900

    헤헤

--- 2636 치즈.cpp
+++ 2636 치즈.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define SIZE 105
-int N, M, f, r, cheese_count;
+int N, M, f, r, cheese;
 int map[SIZE][SIZE], visited[SIZE][SIZE];
 int by[4] = { 0,0,1,-1 }, bx[4] = { 1,-1,0,0 };
 typedef struct { int y; int x; }Queue;
@@ -16,66 +16,44 @@ int empty() {
 	else return 0;
 }
 Queue peek() { return q[f + 1]; }
-void bfs_cheese(int y, int x) {
+int bfs(int y, int x) {
 	f = r = -1;
 	enQ(y, x);
 	visited[y][x] = 1;
+	int count = 1;
 	while (empty() == 0) {
 		int ty = peek().y;
 		int tx = peek().x;
 		deQ();
-		int count = 0;
 		for (int i = 0; i < 4; i++) {
 			int ny = ty + by[i];
 			int nx = tx + bx[i];
-			if (map[ny][nx] == 0 && visited[ny][nx] == 9) {
-				count++;
-			}
-
-			else if (map[ny][nx] == 1 && visited[ny][nx] == 0) {
-				visited[ny][nx] = 1;
-				enQ(ny, nx);
-			}
-		}
-		if (count != 0) {
-			visited[ty][tx] = 5;
-		}
-	}
-}
-int bfs_air() {
-	f = r = -1;
-	enQ(0, 0);
-	visited[0][0] = 9;
-	int cnt = 1;
-	while (empty() == 0) {
-		int ty = peek().y;
-		int tx = peek().x;
-		deQ();
-		int count = 0;
-		for (int i = 0; i < 4; i++) {
-			int ny = ty + by[i];
-			int nx = tx + bx[i];
-			if (0 <= ny && ny < N && 0 <= nx && nx < M&&visited[ny][nx] == 0 && map[ny][nx] == 0) {
-				enQ(ny, nx);
-				visited[ny][nx] = 9;
-				cnt++;
+			if (0 <= ny && ny < N && 0 <= nx && nx < M && visited[ny][nx] == 0) {
+				if (map[ny][nx] == 0) {
+					enQ(ny, nx);
+					visited[ny][nx] = 1;
+					count++;
+				}
+				else {
+					visited[ny][nx] = 9;
+				}
 			}
 		}
 	}
-	return cnt;
+	return count;
 }
 int init() {
-	int cnt = 0;
+	int count = 0;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
-			if (map[i][j] == 1)
-				cnt++;
-			if (visited[i][j] == 5)
+			if (visited[i][j] == 9) {
+				count++;
 				map[i][j] = 0;
+			}
 			visited[i][j] = 0;
 		}
 	}
-	return cnt;
+	return count;
 }
 int main() {
 	freopen("Text.txt", "r", stdin);
@@ -87,18 +65,11 @@ int main() {
 	}
 	int time = 0;
 	while (1) {
-		int temp_count = init();
-		if (temp_count == 0)break;
-		cheese_count = temp_count;
-		if (bfs_air() == N * M)break;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (map[i][j] == 1 && visited[i][j] == 0)
-					bfs_cheese(i, j);
-			}
-		}
+		if (bfs(0, 0) == N * M)
+			break;
+		cheese = init();
 		time++;
 	}
-	printf("%d\n%d\n", time, cheese_count);
+	printf("%d\n%d\n", time, cheese);
 	return 0;
 }
