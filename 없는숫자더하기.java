3fc4b1ab66f9de8c7b8ea57fd3c334d9b987a596
오혜영 <enffl9568@naver.com>
2021-10-14 20:42:35 +0900

    뭐야 이거 레벨 몇이야

---
+++ 없는숫자더하기.java
@@ -0,0 +1,19 @@
+
+public class 없는숫자더하기 {
+
+	static public int solution(int[] numbers) {
+		int answer = 45;
+
+		for (int i = 0; i < numbers.length; i++) {
+			answer -= numbers[i];
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { 5,8,4,0,6,7,9};
+
+		System.out.println(solution(numbers));
+	}
+}
