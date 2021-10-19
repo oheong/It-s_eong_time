d2decca7b05aa0098a59f5fa11c48996cc679b58
OHEONG <enffl9568@naver.com>
2021-10-19 22:08:30 +0900

    어제 틀린부분 찾아냄. 중복되는 path 제거 안해줌
    list.get(edge[i][0]).add(edge[i][1]);
    list.get(edge[i][1]).add(edge[i][0]);
    이거 해주면서 양쪽으로 가도록. 하나를 갔으면 나머지 하나는 제거를 해줬어야했는데 제거안해서 틀림 생각좀해〰〰❗❗🍉❗❗〰🍒❗❗🍒

--- 가장먼노드.java
+++ 가장먼노드.java
@@ -1,25 +1,12 @@
 import java.util.*;
 class Solution {
     static int[] visited;
-    static Queue<Integer> queue, step;
+    static Queue<Integer> queue;
     static ArrayList<ArrayList<Integer>> list;
     public int solution(int n, int[][] edge) {
         int answer = 0;
-        
-        // 이거 미쳤다 ㅎㅎ;ㅎ;ㅎㅎㅎ;ㅎ;ㅎ;ㅎ;ㅎ;ㅎ;ㅎ;ㅎㅎ;ㅎ;ㅎ
-        // 어 근데 굳이 안해도될듯
-        /*
-        Arrays.sort(edge, new Comparator<int[]>(){
-            public int compare(int[] a1, int[] a2){
-                if(a1[0] == a2[0]){
-                    return a1[1]-a2[1];
-                }
-                else return a1[0]-a2[0];
-            }
-        });
-        */
+
         queue = new LinkedList<Integer>();
-        step = new LinkedList<Integer>();
         list = new ArrayList<ArrayList<Integer>>();
         visited = new int[n + 1];
         
@@ -29,31 +16,39 @@ class Solution {
         }
         
         for(int i = 0; i < edge.length; i++){
-            if(edge[i][0] == 1){
-                queue.add(edge[i][1]);  
-                step.add(1);
-                visited[edge[i][1]] = 1;
-            }
             list.get(edge[i][0]).add(edge[i][1]);
-            if(edge[i][0] != 1)
-                list.get(edge[i][1]).add(edge[i][0]);
+            list.get(edge[i][1]).add(edge[i][0]);
         }
+        
+//        for(int i=1;i<n;i++){
+//            System.out.print(i+" : ");
+//            for(int j=0;j<list.get(i).size();j++){
+//                System.out.print(list.get(i).get(j)+" ");
+//            }System.out.println();
+//        }
+        
+        queue.add(1);
+        visited[1] = 1;
         
         int max = 0;
         
         while(!queue.isEmpty()){
             int num = queue.remove();
-            int tstep = step.remove();
+            int tstep = visited[num];
             
-            for(int i = 0; i < list.get(num).size(); i++){
-                if(visited[list.get(num).get(i)] > tstep + 1){
-                    if(max < tstep + 1)max = tstep + 1;
-                    visited[list.get(num).get(i)] = tstep + 1;        
-                    queue.add(list.get(num).get(i));
-                    step.add(tstep + 1);
+            while(!list.get(num).isEmpty()){
+                int next = list.get(num).remove(0);
+                int index = list.get(next).indexOf(num);
+                
+                list.get(next).remove(index); // 중복되는거 제거
+                
+                if(visited[next] > tstep + 1){
+                    if(max < tstep + 1)
+                        max = tstep + 1;
+                    visited[next] = tstep + 1;        
+                    queue.add(next);
                 }
             }
-            
         }
 
         for(int i = 0;i <= n; i++){
