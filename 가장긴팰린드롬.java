9ebf09729f696ff5be316b8d32faa5e6604837a1
OHEONG <enffl9568@naver.com>
2021-10-21 23:04:13 +0900

    시간은 됐는데 6 14 19 효율성1 왜 틀렸노? 아니 도대체 반례뭐란말임

--- 가장긴팰린드롬.java
+++ 가장긴팰린드롬.java
@@ -1,8 +1,8 @@
 class Solution
 {
-    static int getAns(String str){
-        for(int i = 0; i <= str.length() / 2;i++){
-            if(str.charAt(i) != str.charAt(str.length() - i - 1))
+    static int getAns(char[] st, int start, int end){
+        for(int i = 0; i < (end - start + 1) / 2; i++){
+            if(st[start + i] != st[end - i])
                 return 0;
         }
         return 1;
@@ -10,14 +10,16 @@ class Solution
     public int solution(String s)
     {
         int answer = 0;
-
+        char[] st = s.toCharArray();
+        
         for(int start = 0; start <= s.length()/2; start++){
             if(s.length() - start <= answer) return answer;
+           
             for(int end = s.length() - 1; start <= end; end--){
-                if(end - start < answer) break;
-                String str = s.substring(start, end + 1);
-                if(getAns(str) == 1 && answer<str.length()) {
-                    answer = str.length();
+                if(end - start + 1 < answer) break;
+                
+                if(getAns(st, start, end) == 1 && answer < end - start + 1) {
+                    answer = end - start + 1;
                     break;
                 }
             }
